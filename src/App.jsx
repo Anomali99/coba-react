de25147442fc253ca5bcac3bf1459cf87339f999
Egty <egtyramadhanti@gmail.com>
2025-01-26 14:44:44 +0700

    nambah template

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import AboutPage from "./pages/AboutPage";
 
 function App() {
   const [count, setCount] = useState(0);
 
-  return <div className="bg-green-400 min-h-screen min-w-screen">p</div>;
+  return <AboutPage/>;
 }
 
 export default App;
