9a7353389b2ebe127cf58be0ffdcc608cbb61fe8
Anomali99 <fatiqnur@gmail.com>
2025-01-26 12:42:19 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return <div className="bg-green-400 min-h-screen min-w-screen">p</div>;
+}
+
+export default App;
